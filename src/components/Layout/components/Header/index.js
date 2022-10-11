44e20be7003007a61464333b8c840005a6f129a5
hokhanhduycs <hokhanhduycs@gmail.com>
2022-10-11 17:39:35 +0700

    setup children menu

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -24,6 +24,21 @@ const MENU_ITEMS = [
    {
       icon: <FontAwesomeIcon icon={faEarthAsia} />,
       title: 'English',
+      children: {
+         title: 'Language',
+         data: [
+            {
+               type: 'language',
+               code: 'en',
+               title: 'English',
+            },
+            {
+               type: 'language',
+               code: 'vn',
+               title: 'Tiếng Việt',
+            },
+         ],
+      },
    },
    {
       icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -44,6 +59,16 @@ function Header() {
          setSearchResult([])
       }, 0)
    }, [])
+   // handle logic
+   const handleMenuChange = (menuItem) => {
+      switch (menuItem.type) {
+         case 'language':
+            //handle change languge
+            console.log(menuItem)
+            break
+         default:
+      }
+   }
    return (
       <header className={cx('wrapper')}>
          <div className={cx('inner')}>
@@ -84,7 +109,7 @@ function Header() {
                {/* <Button primary leftIcon={<FontAwesomeIcon icon={faSignIn} />}>
                   Log in
                </Button> */}
-               <Menu items={MENU_ITEMS}>
+               <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                   <button className={cx('more-btn')}>
                      <FontAwesomeIcon icon={faEllipsisVertical} />
                   </button>

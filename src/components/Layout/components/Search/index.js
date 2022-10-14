326fc4307d40389ef6f1bf6466e351974ccc6f54
hokhanhduycs <hokhanhduycs@gmail.com>
2022-10-14 14:06:27 +0700

    add debounce

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -11,6 +11,7 @@ import HeadlessTippy from '@tippyjs/react/headless'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import AccountsItem from '~/components/Layout/AccountsItem'
 import styles from './Search.module.scss'
+import { useDebounce } from '~/hooks'
 
 const cx = classNames.bind(styles)
 
@@ -20,17 +21,19 @@ function Search() {
    const [showRusult, setShowRusult] = useState(true)
    const [loading, setLoading] = useState(false)
 
+   const debounced = useDebounce(searchValue, 500)
+
    const inputRef = useRef()
 
    useEffect(() => {
-      if (!searchValue.trim()) {
+      if (!debounced.trim()) {
          setSearchResult([])
          return
       }
       setLoading(true)
       fetch(
          `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-            searchValue
+            debounced
          )}&type=less`
       )
          .then((res) => res.json())
@@ -41,7 +44,7 @@ function Search() {
          .catch(() => {
             setLoading(false)
          })
-   }, [searchValue])
+   }, [debounced])
 
    const handleClear = () => {
       setSearchValue('')
